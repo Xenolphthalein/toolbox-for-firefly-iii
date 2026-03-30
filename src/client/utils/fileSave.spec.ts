cd0f38364e39fa5780caa682d01c7d99cef2d38f
Xenolpthalein <21215869+Xenolphthalein@users.noreply.github.com>
2026-03-30 08:50:17 +0200

    refactor: streamline import statements in fileSave and index utility files

--- src/client/utils/fileSave.spec.ts
+++ src/client/utils/fileSave.spec.ts
@@ -1,8 +1,5 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
-import {
-  sanitizeFilenamePart,
-  saveBlobWithDialog,
-} from './fileSave';
+import { sanitizeFilenamePart, saveBlobWithDialog } from './fileSave';
 
 describe('fileSave', () => {
   beforeEach(() => {
